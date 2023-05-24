f1b4f6b9174faedbda9ab55c3c317db43e88d5f8
Ranjan Kumar <ranjan.wants@gmail.com>
2023-05-24 18:55:54 +0530

    cors fix

--- server/index.js
+++ server/index.js
@@ -31,7 +31,7 @@ app.post("/api/login", (req, res) => {
 
   // Compare the password using bcrypt
   bcrypt.compare(password, user.password, (err, isMatch) => {
-    if (err || !isMatch) {
+    if (err || isMatch) {
       return res.status(401).json({ error: "Invalid username or password" });
     }
 
