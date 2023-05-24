7b023526254d2a763d9938819551efd0ae4c703e
Ranjan Kumar <67799181+ranjan-alt@users.noreply.github.com>
2023-05-24 10:14:26 +0000

    cors fix

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs")
+const cors = require("cors")
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 
 const secretKey = "ranjansah";
