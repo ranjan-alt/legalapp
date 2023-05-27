1b5a651d2595ebc1c95b3c207942bf8153709c66
Ranjan Kumar <ranjan.wants@gmail.com>
2023-05-28 02:27:45 +0530

    cards implemented

--- client/src/components/AdminPanel.jsx
+++ client/src/components/AdminPanel.jsx
@@ -1,67 +1,21 @@
-import { useState } from "react";
-import axios from "axios";
 import { useLocation } from "react-router-dom";
 import Logout from "./Logout";
 import Sidebar from "./sidebar/Sidebar";
 import { Col, Row, Container } from "react-bootstrap";
 import "./main.css";
+import Groupcards from "./GroupCards/GroupCards";
 
 const AdminPanel = () => {
-  const [text, setText] = useState("");
-  const [image, setImage] = useState(null);
-
   const localtion = useLocation();
-
-  const handletextChange = (e) => {
-    setText(e.target.value);
-  };
-
-  const handleImageUpload = (e) => {
-    const file = e.target.files[0];
-    setImage(file);
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    //created payload object
-    const payload = {
-      text,
-      image,
-    };
-    //make post request to server
-
-    try {
-      const response = await axios.post("/file", payload);
-      console.log(response.data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
   return (
     <>
-      <Container className="main">
+      <Container fluid className="main">
         <Row style={{ display: "flex" }}>
           <Col md={3} sm={3} lg={3}>
             <Sidebar />
           </Col>
           <Col md={9} sm={9} lg={9}>
-            <form onSubmit={handleSubmit}>
-              <div>
-                <label htmlFor="">Text</label>
-                <input type="text" onChange={handletextChange} />
-              </div>
-              <div>
-                <label>Image</label>
-                <input
-                  type="file"
-                  accept="image/*"
-                  onChange={handleImageUpload}
-                />
-              </div>
-              <button type="submit">Submit</button>
-            </form>
+            <Groupcards />
             <Logout />
           </Col>
         </Row>
